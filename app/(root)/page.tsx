baa197016087356a43469e73340ba9dd5dcdfd43
OpenLearn <tmoropodi@gmail.com>
2024-08-13 16:59:57 +0200

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import HeaderBox from '@/components/HeaderBox';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import RightSidebar from '@/components/RightSidebar';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const loggedIn = { firstName: 'Moropodi', lastName: 'Ramathebane', email: 'contact@me.com'};
 
-export default Home
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+           type="greeting"
+           title="Welcome,"
+           user={loggedIn?.firstName || 'Gust'}
+           subtext="Access your accounts, manage your finances, and more."
+          />
+
+          <TotalBalanceBox 
+             accounts={[]}
+             totalBanks={1}
+             totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+
+      <RightSidebar 
+      user={loggedIn}
+      transactions={[]}
+      banks={[{ currentBalance: 123.50}, { currentBalance: 500.50}]}
+      />
+    </section>
+  );
+};
+
+export default Home;
